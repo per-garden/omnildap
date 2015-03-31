380e3a27b5240e7400f87be2a7b197824d3d275b
Per Gärdén <per.garden@sonymobile.com>
2015-03-31 17:36:20 +0200

    If user does not exist when ligging in => resort to backend. Hard-coded LDAP for now. Needs cleaning up.

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,5 +1,25 @@
 class Users::SessionsController < Devise::SessionsController
   def create
+    email = params['user']['email']
+    current_user = User.find_by_email(email)
+    unless current_user
+      password = params['user']['password']
+      backend_signup(email, password)
+    end
     super
+  end
+
+  private
+
+  def backend_signup(email, password)
+    # Name for backend is what's in front of the mail domain
+    name = email.split('@')[0]
+    ldap = Net::LDAP.new
+    ldap.port = 10389
+    ldap.base = 'ou=seldlx3031_apache_ds,dc=example,dc=com'
+    ldap.authenticate("cn=#{name},ou=seldlx3031_apache_ds,dc=example,dc=com", password)
+    if ldap.bind
+      User.create!(email: email, name: name, password: password, password_confirmation: password)
+    end
   end
 end
--- lib/custom_failure.rb
+++ lib/custom_failure.rb
@@ -1,7 +1,9 @@
+# Not used - remove this file (see config/initializers/devise.rb)
 class CustomFailure < Devise::FailureApp
   # Failing devise login => LDAP
   def redirect_url
     "#{root_url}" + 'auth/ldap'
+    # "#{root_url}" + 'auth/ldap/callback', {email: email, password: password}, method: :post
   end
 
   # Avoiding recall
@@ -9,7 +11,10 @@ class CustomFailure < Devise::FailureApp
     if http_auth?
       http_auth
     else
-      redirect
+      # redirect
+      email = params[:user][:email]
+      password= params[:user][:password]
+      redirect_to "#{root_url}" + 'auth/ldap/callback', email: email, password: password, method: 'POST'
     end
   end
 end
