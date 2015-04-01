702704f39b4d249ecc4e01b8e1e9aaa60c049d1c
Per Gärdén <per.garden@sonymobile.com>
2015-04-01 17:09:19 +0200

    Activating backends-users associativity.

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,5 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   validates_presence_of :name, :email
+  has_and_belongs_to_many :backends
 end
