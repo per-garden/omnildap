1452cc720fe11f5f9d51ad6016e85af5e34736c7
Per Gärdén <per.garden@sonymobile.com>
2015-04-01 15:39:56 +0200

    Default values for LDAP backend.

--- app/models/ldap_backend.rb
+++ app/models/ldap_backend.rb
@@ -1,2 +1,12 @@
 class LdapBackend < Backend
+  # Cannot guesswork a default entry point into LDAP tree
+  validates_presence_of :base
+  after_initialize :init
+
+  private
+
+  def init
+    self.host ||= 'localhost'
+    self.port ||= 10389
+  end
 end
