6b53efb0c76252ba979e6e91ebf59b81dfba5ab7
Per Gärdén <per.garden@sonymobile.com>
2015-07-03 17:28:40 +0200

    Spec: It lets user log in

--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -1,5 +1,17 @@
 require 'spec_helper'
 
 describe HomeController do
+  describe "GET" do
 
+    before :each do
+      @request.env['devise.mapping'] = Devise.mappings[:user]
+    end
+
+    it 'lets user log in' do
+      user = create(:user)
+      sign_in user
+      get :index, {}, {SERVER_NAME:'localhost:3003'}
+      response.should render_template('home/index')
+    end
+  end
 end
