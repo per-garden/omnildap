cc0beba2b897b5b95a7275ed46c24befcfefb81b
Per Gärdén <per.garden@sonymobile.com>
2015-07-07 18:14:57 +0200

    Restructured ldapdb, ldap_operation properly using basedn as supplied (no longer hard coded).

--- lib/omnildap/ldap_operation.rb
+++ lib/omnildap/ldap_operation.rb
@@ -1,6 +1,6 @@
 module Omnildap
   class LdapOperation < LDAP::Server::Operation
-    @basedn = 'cn=omnildap,dc=omnildap'
+    # @basedn = 'cn=omnildap,dc=omnildap'
 
     class << self
       attr_reader :basedn
@@ -12,15 +12,21 @@ module Omnildap
     end
   
     def search(basedn, scope, deref, filter)
+      # ALog.debug 'Incoming basedn in search is: ' + basedn
       basedn.downcase!
       name = basedn.split(/\W+/)[1]
 
       case scope
       # TODO: single object how/what?
       when LDAP::Server::BaseObject
-        # ALog.debug 'client asked for single object by DN'
+        # ALog.debug 'Client asked for single object by DN. We are now in ldap_operation.rb'
+        # ALog.debug 'We use LdapOperation.basedn ' + basedn
+        # ALog.debug 'to retrieve from hash:'
+        # ALog.debug @hash
         # client asked for single object by DN
-        obj = @hash[LdapOperation.basedn]
+        obj = @hash[basedn]
+        # ALog.debug "And this is what we return:"
+        # ALog.debug obj
         raise LDAP::ResultError::NoSuchObject unless obj
         send_SearchResultEntry(basedn, obj) if LDAP::Server::Filter.run(filter, obj)
   
