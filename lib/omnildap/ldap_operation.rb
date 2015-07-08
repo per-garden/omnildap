c30849b34ca76524a0f0789cf53e89e86e1f6eeb
Per Gärdén <per.garden@sonymobile.com>
2015-07-08 11:48:27 +0200

    Correct scoping in ldap operation. Only base and sub implemented.

--- lib/omnildap/ldap_operation.rb
+++ lib/omnildap/ldap_operation.rb
@@ -12,31 +12,32 @@ module Omnildap
     end
   
     def search(basedn, scope, deref, filter)
-      # ALog.debug 'Incoming basedn in search is: ' + basedn
+      # ALog.debug 'Scope of search is:'
+      # ALog.debug scope.to_i.to_s
       basedn.downcase!
       name = basedn.split(/\W+/)[1]
 
-      case scope
-      # TODO: single object how/what?
-      when LDAP::Server::BaseObject
-        # ALog.debug 'Client asked for single object by DN. We are now in ldap_operation.rb'
-        # ALog.debug 'We use LdapOperation.basedn ' + basedn
-        # ALog.debug 'to retrieve from hash:'
-        # ALog.debug @hash
-        # client asked for single object by DN
+      # Scope base, one, sub or children, specifying base object, one-level,
+      # or subtree search (children requires LDAPv3 subordinate feature extension)
+      # (http://www.zytrax.com/books/ldap/ch14/#ldapsearch)
+      case scope.to_i
+      when 0 #LDAP::Server::BaseObject
+        # Client asked for single object by DN
         obj = @hash[basedn]
-        # ALog.debug "And this is what we return:"
-        # ALog.debug obj
         raise LDAP::ResultError::NoSuchObject unless obj
         send_SearchResultEntry(basedn, obj) if LDAP::Server::Filter.run(filter, obj)
-  
-      when LDAP::Server::WholeSubtree
-        u = User.find_by_name(name)
-        send_SearchResultEntry(basedn, {'cn' => [u.name], 'mail' => [u.email]}) if u
-  
-      else
+      when 1
         raise LDAP::ResultError::UnwillingToPerform, "OneLevel not implemented"
-  
+      when 2
+        basedn = @hash.keys[0]
+        obj = @hash[basedn]
+        send_SearchResultEntry(basedn, obj) if LDAP::Server::Filter.run(filter, obj)
+        # u = User.find_by_name(name)
+        # send_SearchResultEntry(basedn, {'cn' => [u.name], 'mail' => [u.email]}) if u
+      when 3
+        raise LDAP::ResultError::UnwillingToPerform, "Children not implemented"
+      else
+        raise LDAP::ResultError::UnwillingToPerform, "Unimplemented scope. Must be base or sub."
       end
     end
   
