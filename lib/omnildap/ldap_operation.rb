4d74440bc8462e82a6ee274a54324a11c2d47c26
Per Gärdén <per.garden@sonymobile.com>
2015-07-08 16:20:25 +0200

    Cleaning up

--- lib/omnildap/ldap_operation.rb
+++ lib/omnildap/ldap_operation.rb
@@ -1,7 +1,5 @@
 module Omnildap
   class LdapOperation < LDAP::Server::Operation
-    # FIXME: Base from configuration
-    @basedn = 'dc=omnildap'
 
     class << self
       attr_reader :basedn
@@ -9,7 +7,9 @@ module Omnildap
 
     def initialize(connection, messageID, hash = {})
       super(connection, messageID)
-      @hash = {}
+      # FIXME: Base from configuration
+      @basedn = 'dc=omnildap'
+      @hash = hash
       User.all.each do |u|
         entry = {}
         entry['cn'] = u.name
@@ -18,22 +18,22 @@ module Omnildap
       end
     end
   
-    def search(basedn, scope, deref, filter)
+    def search(basedn, scope, deref, filter = [:true])
       basedn.downcase!
-      name = basedn.split(/\W+/)[1]
 
       # Scope base, one, sub or children, specifying base object, one-level,
       # or subtree search (children requires LDAPv3 subordinate feature extension)
       # (http://www.zytrax.com/books/ldap/ch14/#ldapsearch)
       case scope.to_i
-      when 0 #LDAP::Server::BaseObject
-        # Client asked for single object by DN
+      when 0
+        # Base object
         obj = @hash[basedn]
         raise LDAP::ResultError::NoSuchObject unless obj
         send_SearchResultEntry(basedn, obj) if LDAP::Server::Filter.run(filter, obj)
       when 1
         raise LDAP::ResultError::UnwillingToPerform, "OneLevel not implemented"
       when 2
+        # Subtree
         @hash.keys.each do |key|
           obj = @hash[key]
           send_SearchResultEntry(basedn, obj) if obj && LDAP::Server::Filter.run(filter, obj)
--- lib/omnildap/ldap_server.rb
+++ lib/omnildap/ldap_server.rb
@@ -4,16 +4,11 @@ module Omnildap
   
     def self.start
       unless @@server
-        directory = {}
-        # FIXME: Ldap data to be read dynamically - not hard coded from file.
-        File.open("lib/omnildap/ldapdb.yaml") { |f| directory = YAML::load(f.read) }
         params = Rails.application.config.ldap_server
         params[:operation_class] = Omnildap::LdapOperation
-        # params[:operation_args] = [directory]
         @@server = LDAP::Server.new(params)
         @@server.run_tcpserver
         # ALog.debug 'Started tcp_server'
-        # Omnildap::LdapServerCounter.increment
       end
     end
   
@@ -21,7 +16,6 @@ module Omnildap
       if @@server
         @@server.stop
         # ALog.debug 'Stopped tcp_server'
-        # Omnildap::LdapServerCounter.decrement
       end
     end
   end
