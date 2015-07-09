a57cacfbf13ca785810e64144dc512da739f3c6f
Per Gärdén <per.garden@sonymobile.com>
2015-07-09 11:12:50 +0200

    Ldap server properly logging in sidekiq.log. How to use existing logger formatter? Separate log file for LDAP?

--- lib/omnildap/ldap_server.rb
+++ lib/omnildap/ldap_server.rb
@@ -8,14 +8,16 @@ module Omnildap
         params[:operation_class] = Omnildap::LdapOperation
         @@server = LDAP::Server.new(params)
         @@server.run_tcpserver
-        # ALog.debug 'Started tcp_server'
+        message = 'Started listening on port ' + "#{Rails.application.config.ldap_server[:port]}"
+        puts  "#{Time.now.utc.iso8601} #{Process.pid} TID-#{Thread.current.object_id.to_s(36)} Omnildap::LdapServer INFO: #{message}\n" 
       end
     end
   
     def self.stop
       if @@server
         @@server.stop
-        # ALog.debug 'Stopped tcp_server'
+        message = 'Stopped listening on port ' + "#{Rails.application.config.ldap_server[:port]}"
+        puts  "#{Time.now.utc.iso8601} #{Process.pid} TID-#{Thread.current.object_id.to_s(36)} Omnildap::LdapServer INFO: #{message}\n" 
       end
     end
   end
