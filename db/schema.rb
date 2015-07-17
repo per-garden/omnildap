baa387405ac952a0900fe22042daa68fb9318cf8
Per Gärdén <per.garden@sonymobile.com>
2015-07-17 12:48:43 +0200

    DB Schema oops one comma too many...

--- db/schema.rb
+++ db/schema.rb
@@ -49,7 +49,7 @@ ActiveRecord::Schema.define(version: 20150717090308) do
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
     t.string   "name"
-    t.boolean  "admin",
+    t.boolean  "admin"
     t.boolean  "blocked"
   end
 
