7f8bd202a0c5fdd23f99193d925eb04d2ca7fc48
Per Gärdén <per.garden@sonymobile.com>
2015-08-12 16:34:33 +0200

    Working backend update (controller & GUI)

--- app/controllers/backends_controller.rb
+++ app/controllers/backends_controller.rb
@@ -12,4 +12,40 @@ class BackendsController < ApplicationController
   def edit
     @backend = Backend.find(params[:id])
   end
+
+  def update
+    ALog.debug params
+    type = params.keys[3]
+    case type
+    when 'devise_backend'
+      backend = Backend.find(params[:id])
+      backend.update(devise_backend_params)
+    when 'ldap_backend'
+      backend = Backend.find(params[:id])
+      backend.update(ldap_backend_params)
+    when 'active_directory_backend'
+      backend = Backend.find(params[:id])
+      backend.update(active_directory_backend_params)
+    end
+
+    if backend && backend.save
+      redirect_to backend_path(backend), notice: 'Authentication backend was updated.'
+    else
+      redirect_to backends_path
+    end
+  end
+
+  private
+
+  def devise_backend_params
+    params.require(:devise_backend).permit(:name, :description, :blocked, :email_pattern, :signup_enabled)
+  end
+
+  def ldap_backend_params
+    params.require(:ldap_backend).permit(:name, :description, :host, :base, :port, :admin_name, :admin_password, :blocked, :email_pattern)
+  end
+
+  def active_directory_backend_params
+    params.require(:active_directory_backend).permit(:name, :description, :host, :base, :port, :admin_name, :admin_password, :blocked, :email_pattern)
+  end
 end
