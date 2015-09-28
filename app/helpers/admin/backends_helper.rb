6d2d73e7c092de0d7c2d13053410acf8eafa0105
Per Gärdén <per.garden@sonymobile.com>
2015-09-28 12:14:13 +0200

    Helper to turn off styling in drop-down. In css instead?

---
+++ app/helpers/admin/backends_helper.rb
@@ -0,0 +1,7 @@
+module Admin::BackendsHelper
+
+  # In (e.g.) an action-item-styled drop down, reset to neutral
+  def dropdown_link_style
+   "color: #555; background-color: transparent; border-color: transparent;"
+  end
+end
