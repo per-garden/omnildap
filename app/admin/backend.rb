a388a2f4b267cb7f3bf67ed8979e898bde265054
Per Gärdén <per.garden@sonymobile.com>
2015-09-28 14:03:01 +0200

    Controller create for admin backend.

--- app/admin/backend.rb
+++ app/admin/backend.rb
@@ -49,11 +49,44 @@ ActiveAdmin.register Backend do
       @page_title = "New #{@backend.type}"
     end
 
+    def create
+      type = params.keys[2]
+      case type
+      when 'devise_backend'
+        # Singleton!
+        backend = DeviseBackend.instance
+        backend.update(devise_backend_params)
+      when 'ldap_backend'
+        backend = LdapBackend.new(ldap_backend_params)
+      when 'active_directory_backend'
+        backend = ActiveDirectoryBackend.new(active_directory_backend_params)
+      end
+  
+      if backend && backend.save!
+        redirect_to admin_backend_path(backend), notice: 'Authentication backend was created.'
+      else
+        redirect_to admin_backends_path
+      end
+    end
+  
     private
-
+  
     def authenticate_user!
       redirect_to(root_path) unless current_user && current_user.admin
     end
+
+    def devise_backend_params
+      params.require(:devise_backend).permit(:name, :description, :blocked, :email_pattern, :signup_enabled)
+    end
+  
+    def ldap_backend_params
+      params.require(:ldap_backend).permit(:name, :description, :host, :base, :port, :admin_name, :admin_password, :blocked, :email_pattern)
+    end
+  
+    def active_directory_backend_params
+      params.require(:active_directory_backend).permit(:name, :description, :host, :base, :port, :admin_name, :admin_password, :blocked, :email_pattern, :domain)
+    end
+
   end
 
 end
