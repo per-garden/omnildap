b01d36d08e3a72dbf7b96041abf071913870e4ce
Per Garden <per.garden@avalonenterprise.com>
2015-10-01 17:43:16 +0200

    So why doesn't sidekiqctl work properly?

--- config/initializers/ldap_server.rb
+++ config/initializers/ldap_server.rb
@@ -9,7 +9,16 @@ if !$rails_rake_task
   at_exit do
     Omnildap::LdapServer.stop
     if File.exists?("#{Rails.root}/tmp/pids/sidekiq.pid")
-      `bundle exec sidekiqctl stop "#{Rails.root}"/tmp/pids/sidekiq.pid >> "#{Rails.root}"/log/sidekiq.log 2>&1`
+      # `bundle exec sidekiqctl stop "#{Rails.root}"/tmp/pids/sidekiq.pid >> "#{Rails.root}"/log/sidekiq.log 2>&1`
+      # So why doesn't sidekiqctl work properly?
+      file = File.open("#{Rails.root}/tmp/pids/sidekiq.pid", "r")
+      pid = file.read.gsub(/[^0-9]/,'')
+      file.close
+      `kill "#{pid}" >> "#{Rails.root}"/log/sidekiq.log 2>&1`
+      while  File.exists?("#{Rails.root}/tmp/pids/sidekiq.pid")
+        File.delete("#{Rails.root}/tmp/pids/sidekiq.pid")
+        sleep(1)
+      end
     end
     silence_stream(STDERR) do
       unless `spring status`.start_with?('Spring is not running')
